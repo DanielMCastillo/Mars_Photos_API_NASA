7d035293d53f374561b97eb3044c4ef3e61fffb5
Daniel M Castillo <danielalejandromoralescastillo@gmail.com>
2025-06-03 14:03:40 -0600

    index and about finished

--- nasa_rover_api_project/nasa_app/views.py
+++ nasa_rover_api_project/nasa_app/views.py
@@ -4,4 +4,8 @@ from .nasa_api import get_mars_photos
 def mars_photos_view(request):
     earth_date = request.GET.get('date', '2020-07-01')  # Puedes cambiar la fecha base
     photos = get_mars_photos(earth_date)
-    return render(request, "nasa_app/base.html", {'photos': photos, 'date': earth_date})
+    return render(request, "nasa_app/index.html", {'photos': photos, 'date': earth_date})
+
+def mars_about(request):
+    return render(request, "nasa_app/about.html")
+    
